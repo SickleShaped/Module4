f73e96e51cd051d9258e2d976f34aef4bf7aa702
Филиппов Алексей <teenwizard.fornewvideo@gmail.com>
2024-03-16 12:28:36 +0300

    arraysorting

--- Program.cs
+++ Program.cs
@@ -6,13 +6,34 @@ namespace module4
     {
         public static void Main(string[] args)
         {
-
-            Console.WriteLine("Введите имя");
-            string name = Console.ReadLine();
-
-            for(int i = name.Length-1; i>=0; i--)
+            var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
+            int i = 0;
+            while (true)
             {
-                Console.Write(name[i]+" ");
+                
+                if (i < arr.Length-1)
+                {
+                    if (arr[i] > arr[i+1])
+                    {
+                        int x = arr[i + 1];
+                        arr[i+1] = arr[i];
+                        arr[i] = x;
+                        i = 0 ;
+                    }
+                     else i++; 
+                }
+
+                if (i == arr.Length - 2  && arr[i] > arr[i+1])
+                {
+                    int x = arr[i + 1];
+                    arr[i + 1] = arr[i];
+                    arr[i] = x;
+                    break;
+                }
+            }
+            foreach (var a in arr)
+            {
+                Console.Write(a + " ");
             }
 
         }
