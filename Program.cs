4c15561ed14e0ccd2a0db677345b49a90a560512
Филиппов Алексей <teenwizard.fornewvideo@gmail.com>
2024-03-16 12:50:49 +0300

    matrix sort

--- Program.cs
+++ Program.cs
@@ -7,16 +7,22 @@ namespace module4
         public static void Main(string[] args)
         {
             int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
-            int natural = 0;
-            foreach (var str in arr)
-            {
 
-                    if (str >= 0) { natural++; }
-                
+            int temp;
+            for(int i = 0; i < arr.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j < arr.GetUpperBound(1); j++)
+                    for (int k = j + 1; k<arr.GetUpperBound(1);k++)       
+                        if(arr[i,j] > arr[i,k])
+                        {
+                            temp = arr[i,k];
+                            arr[i, k] = arr[i, j];
+                            arr[i,j] = temp;
+                        }
             }
 
-
-            Console.WriteLine(natural);
+            foreach(var a in arr)
+            { Console.WriteLine(a); }
 
         }
     }
