0042b68f7f2c00ad433c4e3cdb05dffd100abf2b
Филиппов Алексей <teenwizard.fornewvideo@gmail.com>
2024-03-16 11:04:04 +0300

    Added case Cyan

--- Program.cs
+++ Program.cs
@@ -2,34 +2,43 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-
-            var color = Console.ReadLine();
-
-            if (color == "red")
+            public static void Main(string[] args)
             {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-                Console.WriteLine("Your color is red!");
+                var color = Console.ReadLine();
+
+                switch (color)
+                {
+                    case "red":
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is red!");
+                        break;
+
+                    case "green":
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is green!");
+                        break;
+
+                    case "cyan":
+                        Console.BackgroundColor = ConsoleColor.Cyan;
+                        Console.ForegroundColor = ConsoleColor.Black;
+
+                        Console.WriteLine("Your color is cyan!");
+                        break;
+
+                    default:
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine("Your color is default!");
+                        break;
+                }
             }
-
-            else if (color == "green")
-            {
-                Console.BackgroundColor = ConsoleColor.Green;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is green!");
-            }
-            else
-            {
-                Console.BackgroundColor = ConsoleColor.Cyan;
-                Console.ForegroundColor = ConsoleColor.Black;
-
-                Console.WriteLine("Your color is cyan!");
-            }
-        }
+    
     }
 }
