30fa21823cc91273131da2d8fe84d2ea6b3a60b6
Филиппов Алексей <teenwizard.fornewvideo@gmail.com>
2024-03-16 12:37:09 +0300

    summ array

--- Program.cs
+++ Program.cs
@@ -7,34 +7,12 @@ namespace module4
         public static void Main(string[] args)
         {
             var arr = new int[] { 5, 6, 9, 1, 2, 3, 4 };
-            int i = 0;
-            while (true)
+            int summ = 0;
+            foreach (var item in arr)
             {
-                
-                if (i < arr.Length-1)
-                {
-                    if (arr[i] > arr[i+1])
-                    {
-                        int x = arr[i + 1];
-                        arr[i+1] = arr[i];
-                        arr[i] = x;
-                        i = 0 ;
-                    }
-                     else i++; 
-                }
-
-                if (i == arr.Length - 2  && arr[i] > arr[i+1])
-                {
-                    int x = arr[i + 1];
-                    arr[i + 1] = arr[i];
-                    arr[i] = x;
-                    break;
-                }
+                summ += item;
             }
-            foreach (var a in arr)
-            {
-                Console.Write(a + " ");
-            }
+            Console.WriteLine(summ);
 
         }
     }
