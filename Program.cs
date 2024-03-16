2624a762b2e4a7b02dedae10ac26c34d6b564c82
Филиппов Алексей <teenwizard.fornewvideo@gmail.com>
2024-03-16 11:53:42 +0300

    reverse name console

--- Program.cs
+++ Program.cs
@@ -1,44 +1,20 @@
-﻿namespace module4
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace module4
 {
     internal class Program
     {
-            public static void Main(string[] args)
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine();
+
+            for(int i = name.Length-1; i>=0; i--)
             {
-                Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
-
-                var color = Console.ReadLine();
-
-                switch (color)
-                {
-                    case "red":
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is red!");
-                        break;
-
-                    case "green":
-                        Console.BackgroundColor = ConsoleColor.Green;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is green!");
-                        break;
-
-                    case "cyan":
-                        Console.BackgroundColor = ConsoleColor.Cyan;
-                        Console.ForegroundColor = ConsoleColor.Black;
-
-                        Console.WriteLine("Your color is cyan!");
-                        break;
-
-                    default:
-                        Console.BackgroundColor = ConsoleColor.Yellow;
-                        Console.ForegroundColor = ConsoleColor.Red;
-
-                        Console.WriteLine("Your color is default!");
-                        break;
-                }
+                Console.Write(name[i]+" ");
             }
-    
+
+        }
     }
 }
